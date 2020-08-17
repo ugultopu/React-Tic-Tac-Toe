696231aba288a36d6cfeeb3597fa776e09a95fad
Utku Gultopu <ugultopu@gmail.com>
2020-08-17 10:34:59 -0300

    Updates

---
+++ precache-manifest.b408e9cc8aad69c8e5827e43ea5ea89e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da52e043271c4b48a93b77ffa868514",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "004876ff80ebebdc2c3f",
+    "url": "/react-tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "7797abb59617da9e4b2e",
+    "url": "/react-tic-tac-toe/static/js/2.081d35d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.081d35d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004876ff80ebebdc2c3f",
+    "url": "/react-tic-tac-toe/static/js/main.e4f38d6f.chunk.js"
+  },
+  {
+    "revision": "09962ff422a3e1e4ff6e",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.589ee1a9.js"
+  }
+]);
