a78bb7e62a66cbba11b322b1a1c67aa2cb5130c9
Utku Gultopu <ugultopu@gmail.com>
2020-08-22 01:56:51 -0300

    Put "static" before static methods

--- src/Game.js
+++ src/Game.js
@@ -7,34 +7,78 @@ import {
 } from 'arrayUtils';
 
 class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.validateProps();
-    const {boardDimensions: {width, height}} = this.props;
-    this.state = {
-      // Array of indices. The indices refer to an array that
-      // represents the game board (that is, a matrix).
-      moves: [],
-      stepNumber: 0,
-      // NOTE: 'squares' is derived from 'moves'.
-      squares: Array(height * width).fill(null),
-    };
-  }
-
-  validateProps() {
-    const {numElementsRequiredForWin, boardDimensions} = this.props;
-    if (
-      Math.max(...Object.values(numElementsRequiredForWin))
-      >
-      Math.min(...Object.values(boardDimensions))
-    ) {
-      throw new RangeError("Number of elements required to win " +
-                           "cannot be bigger than board dimensions.");
-    }
-  }
 
   static getMovesUntilStep(moves, stepNumber) {
     return moves.slice(0, stepNumber);
+  }
+
+  /*
+   * Returns [rowIndex, columnIndex] representation of a regular array
+   * index, where the array represents a matrix (that is, a
+   * two-dimensional array).
+   */
+  static getPointForMove(move, {width}) {
+    return [move % width, Math.floor(move / width)];
+  }
+
+  static getLastPlayerMovesAsPoints(moves, boardDimensions) {
+    const isLastPlayerX = moves.length % 2 !== 0,
+          lastPlayerMovesAsPoints = [];
+    for (let i = isLastPlayerX ? 0 : 1; i < moves.length; i+=2) {
+      lastPlayerMovesAsPoints.push(
+        Game.getPointForMove(moves[i], boardDimensions)
+      );
+    }
+    return lastPlayerMovesAsPoints;
+  }
+
+  static isPointValid(point, {width, height}) {
+    return (
+      !(point[0] < 0) && (point[0] < width)
+      &&
+      !(point[1] < 0) && (point[1] < height)
+    );
+  }
+
+  static getWinningEndpointsForDirection({
+    direction,
+    moves,
+    boardDimensions,
+    numElementsRequiredForWin,
+  }) {
+    const lastPlayerMoves = Game.getLastPlayerMovesAsPoints(
+                              moves,
+                              boardDimensions
+                            ),
+          target = numElementsRequiredForWin[direction],
+          delta = Board.directionDeltas[direction],
+          lastMove = lastPlayerMoves[lastPlayerMoves.length - 1],
+          endpoints = [];
+    let numElements = 1;
+    for (let i = 0; i < 2; i++) {
+      const multiplier = (i === 0 ? -1 : 1);
+      let point = sumArrays(lastMove, delta, multiplier);
+      while (Game.isPointValid(point, boardDimensions)
+            && isArrayInArrayOfArrays(lastPlayerMoves, point)
+      ) {
+        numElements++;
+        endpoints[i] = point;
+        point = sumArrays(point, delta, multiplier);
+      }
+    }
+    if (numElements === target) return endpoints;
+  }
+
+  static getWinningEndpoints(args) {
+    const winningEndpoints = [];
+    for (const direction in Board.directionDeltas) {
+      const endpoints = Game.getWinningEndpointsForDirection({
+                          direction,
+                          ...args,
+                        });
+      if (endpoints) winningEndpoints.push(endpoints);
+    }
+    return winningEndpoints;
   }
 
   static getSquaresFromMoves(moves, previousStepNumber, stepNumber, squares) {
@@ -54,91 +98,71 @@ class Game extends React.Component {
     return squares;
   }
 
-  /*
-   * Returns [rowIndex, columnIndex] representation of a regular array
-   * index, where the array represents a matrix (that is, a
-   * two-dimensional array).
-   */
-  getPointForMove(move) {
-    const {width} = this.props.boardDimensions;
-    return [move % width, Math.floor(move / width)];
+  constructor(props) {
+    super(props);
+    this.validateProps();
+    const {boardDimensions: {width, height}} = this.props;
+    this.state = {
+      // Array of indices. The indices refer to an array that
+      // represents the game board (that is, a matrix).
+      moves: [],
+      stepNumber: 0,
+      // NOTE: 'squares' is derived from 'moves'.
+      squares: Array(height * width).fill(null),
+      // NOTE: 'winningEndpoints is derived from 'moves.
+      // Array of tuples (a tuple is an array of two elements).
+      winningEndpoints: [],
+    };
   }
 
-  getLastPlayerMovesAsPoints() {
-    const {moves} = this.state,
-          isLastPlayerX = moves.length % 2 !== 0,
-          lastPlayerMovesAsPoints = [];
-    for (let i = isLastPlayerX ? 0 : 1; i < moves.length; i+=2) {
-      lastPlayerMovesAsPoints.push(this.getPointForMove(moves[i]));
+  validateProps() {
+    const {numElementsRequiredForWin, boardDimensions} = this.props;
+    if (
+      Math.max(...Object.values(numElementsRequiredForWin))
+      >
+      Math.min(...Object.values(boardDimensions))
+    ) {
+      throw new RangeError("Number of elements required to win " +
+                           "cannot be bigger than board dimensions.");
     }
-    return lastPlayerMovesAsPoints;
-  }
-
-  isPointValid(point) {
-    const {width, height} = this.props.boardDimensions;
-    return (
-      !(point[0] < 0) && (point[0] < width)
-      &&
-      !(point[1] < 0) && (point[1] < height)
-    );
-  }
-
-  getWinningEndpointsForDirection(direction) {
-    const lastPlayerMoves = this.getLastPlayerMovesAsPoints(),
-          target = this.props.numElementsRequiredForWin[direction],
-          delta = Board.directionDeltas[direction],
-          lastMove = lastPlayerMoves[lastPlayerMoves.length - 1],
-          endpoints = [];
-    let numElements = 1;
-    for (let i = 0; i < 2; i++) {
-      const multiplier = (i === 0 ? -1 : 1);
-      let point = sumArrays(lastMove, delta, multiplier);
-      while (this.isPointValid(point)
-            && isArrayInArrayOfArrays(lastPlayerMoves, point)
-      ) {
-        numElements++;
-        endpoints[i] = point;
-        point = sumArrays(point, delta, multiplier);
-      }
-    }
-    if (numElements === target) return endpoints;
-  }
-
-  // Array of tuples (a tuple is an array of two elements).
-  get winningEndpoints() {
-    const winningEndpoints = [];
-    for (const direction in Board.directionDeltas) {
-      const endpoints = this.getWinningEndpointsForDirection(direction);
-      if (endpoints) winningEndpoints.push(endpoints);
-    }
-    return winningEndpoints;
   }
 
   isGameEnded() {
-    const {moves, stepNumber} = this.state;
-    return  moves.length > 0
-            &&
-            stepNumber === moves.length
-            &&
-            this.winningEndpoints.length > 0;
+    const {moves, stepNumber, winningEndpoints} = this.state;
+    return stepNumber === moves.length && winningEndpoints.length > 0;
   }
 
   addMove = move => {
-    this.setState(({moves, stepNumber, squares}) => {
-      if (moves.includes(move)) return;
+    this.setState((
+      {moves, stepNumber, squares},
+      {boardDimensions, numElementsRequiredForWin}
+    ) => {
+      const previousMoves = Game.getMovesUntilStep(moves, stepNumber);
+      if (previousMoves.includes(move)) return;
+      moves = [...previousMoves, move];
       squares[move] = stepNumber % 2 === 0 ? 'X' : 'O';
       return {
-        moves: [...Game.getMovesUntilStep(moves, stepNumber), move],
+        moves,
         stepNumber: stepNumber + 1,
         squares,
+        winningEndpoints: Game.getWinningEndpoints({
+          moves,
+          boardDimensions,
+          numElementsRequiredForWin,
+        }),
       }
     })
   }
 
   jumpTo = newStepNumber => {
     this.setState(({moves, stepNumber, squares}) => ({
-        stepNumber: newStepNumber,
-        squares: Game.getSquaresFromMoves(moves, stepNumber, newStepNumber, squares),
+      stepNumber: newStepNumber,
+      squares: Game.getSquaresFromMoves(
+                 moves,
+                 stepNumber,
+                 newStepNumber,
+                 squares
+               ),
     }));
   }
 
@@ -174,6 +198,7 @@ class Game extends React.Component {
       </div>
     );
   }
+
 }
 
 export default Game;
